0054554fef7cf439ee503decf16864dabb0fbf76
Brent Baumgartner <brent@brentbaum.com>
2019-03-24 01:02:27 -0400

    Yeah

--- env.sh
+++ env.sh
@@ -3,12 +3,11 @@ function join_by { local IFS="$1"; shift; echo "$*"; }
 joined=''
 
 for s in $(jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" .env); do
-  joined=$(join_by ' ' $joined $s)
+  if [ "$1" = "remote" ]; then
+    gigalixir config:set $s
+  fi
   export $s
 done
 
 echo $joined
 
-if [ "$1" = "remote" ]; then
-  gigalixir config:set $joined
-fi
