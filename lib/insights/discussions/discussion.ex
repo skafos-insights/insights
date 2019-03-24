3955853b4a33c9c5ce63f446d8d4e3ec22fb66a6
Brent Baumgartner <brent@brentbaum.com>
2019-03-24 01:47:24 -0400

    Add default sorting by # of discussions.

--- lib/insights/discussions/discussion.ex
+++ lib/insights/discussions/discussion.ex
@@ -3,24 +3,23 @@ defmodule Insights.Discussions.Discussion do
   import Ecto.Changeset
 
   schema "discussions" do
-    field :absent, :string
-    field :present, :string
-    field :body, :string
-    field :votes, :string
-    field :type, :string
-    field :discussion_type, :string
-    field :dollar_amount, :string
-    field :status, :string
-    belongs_to :meeting, Insights.Meetings.Meeting, foreign_key: :meeting_id
-    belongs_to :issue, Insights.Issues.Issue, foreign_key: :issue_id
+    field(:absent, :string)
+    field(:present, :string)
+    field(:body, :string)
+    field(:votes, :string)
+    field(:type, :string)
+    field(:discussion_type, :string)
+    field(:dollar_amount, :string)
+    field(:status, :string)
+    belongs_to(:meeting, Insights.Meetings.Meeting, foreign_key: :meeting_id)
+    belongs_to(:issue, Insights.Issues.Issue, foreign_key: :issue_id)
 
     timestamps()
   end
 
-  @doc false
   def changeset(discussion, attrs) do
     discussion
-    |> cast(attrs, [:body, :meeting_id, :issue_id])
+    |> cast(attrs, [:body, :status, :votes, :meeting_id, :issue_id, :discussion_type, :type])
     |> validate_required([:body, :meeting_id, :issue_id])
   end
 end
