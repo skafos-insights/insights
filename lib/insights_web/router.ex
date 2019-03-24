f3e7fbbebe55b203d8dea3b7ef9f14111b271d45
Brent Baumgartner <brent@brentbaum.com>
2019-03-24 00:09:27 -0400

    Disable exterminate

--- lib/insights_web/router.ex
+++ lib/insights_web/router.ex
@@ -2,39 +2,39 @@ defmodule InsightsWeb.Router do
   use InsightsWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", InsightsWeb.Api, as: :api do
-    pipe_through :api
+    pipe_through(:api)
 
-    resources "/issues", IssueController
+    resources("/issues", IssueController)
     # resources "/members", MemberController
-    resources "/discussions", DiscussionController
-    resources "/meetings", MeetingController
+    resources("/discussions", DiscussionController)
+    resources("/meetings", MeetingController)
     # resources "/votes", VoteController
   end
 
   scope "/", InsightsWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
-    get "/sample", SampleController, :index
-    resources "/issues", IssueController
-    resources "/members", MemberController
-    resources "/discussions", DiscussionController
-    resources "/meetings", MeetingController
-    post "/meetings/import", MeetingController, :import
-    resources "/votes", VoteController
-    get("/exterminate", PageController, :exterminate)
+    get("/", PageController, :index)
+    get("/sample", SampleController, :index)
+    resources("/issues", IssueController)
+    resources("/members", MemberController)
+    resources("/discussions", DiscussionController)
+    resources("/meetings", MeetingController)
+    post("/meetings/import", MeetingController, :import)
+    resources("/votes", VoteController)
+    # get("/exterminate", PageController, :exterminate)
   end
 
   # Other scopes may use custom stacks.
